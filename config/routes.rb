2ec37cfe11e69596edf764574a9f1b1dffdd7647
Killian Talin <killian.talin@live.fr>
2017-09-07 12:26:52 +0530

    Create Ride controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'ride/index'
+
+  get 'ride/show'
+
+  get 'ride/new'
+
+  get 'ride/create'
+
+  get 'ride/edit'
+
+  get 'ride/update'
+
+  get 'ride/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
