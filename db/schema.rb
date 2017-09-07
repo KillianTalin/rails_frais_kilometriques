b706a05b6781bca02182ac8bc51e5f95c41271ee
Killian Talin <killian.talin@live.fr>
2017-09-07 12:13:28 +0530

    Add the Ride model + add first_name & last_name to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907060432) do
+ActiveRecord::Schema.define(version: 20170907063603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rides", force: :cascade do |t|
+    t.date     "date"
+    t.string   "reason"
+    t.string   "start_place"
+    t.string   "end_place"
+    t.integer  "ride_kilometer"
+    t.boolean  "two_way_trip"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["user_id"], name: "index_rides_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,8 +41,11 @@ ActiveRecord::Schema.define(version: 20170907060432) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "rides", "users"
 end
