1f83b04aa53474c4ef374922fed2927dad1048ac
Killian Talin <killian.talin@live.fr>
2017-09-08 00:46:45 +0530

    Creation of a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Ride.destroy_all
+User.destroy_all
+
+puts 'Creating users...'
+users_attributes = [
+  {
+    email:      "killian.talin@gmail.com",
+    password:   "123456",
+    first_name: "Killian",
+    last_name:  "Talin",
+  }
+]
+User.create!(users_attributes)
+
+puts 'Creating rides...'
+rides_attributes = [
+  {
+    date:           20170709,
+    reason:         "Achat de cartouches d'encre au magasin",
+    start_place:    "16 rue Villa Gaudelet, 75011 Paris",
+    end_place:      "10 rue de la paix, 75002 Paris",
+    ride_kilometer: 20,
+    two_way_trip:   true,
+    user:           User.first
+  },
+  {
+    date:           20170809,
+    reason:         "Trajet retour Le Wagon / Maison",
+    start_place:    "16 rue Villa Gaudelet, 75011 Paris",
+    end_place:      "20 avenue de Lespinasse, 93250 Villemomble",
+    ride_kilometer: 12,
+    two_way_trip:   false,
+    user:           User.first
+  }
+]
+Ride.create!(rides_attributes)
+
+puts 'Finished!'
