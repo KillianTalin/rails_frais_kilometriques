054a8113272a128442ce1f4e1bcb47a5a129307b
Killian Talin <killian.talin@live.fr>
2017-09-14 13:13:42 +0530

    End of Distance Matrix - Google

--- app/assets/javascripts/distance_matrix.js
+++ app/assets/javascripts/distance_matrix.js
@@ -1,5 +1,5 @@
-const newRideForm = document.querySelector("#new_ride");
-newRideForm.addEventListener("click", (event) => {
+const addressFrom = document.querySelector("#ride_start_place, #ride_end_place");
+addressFrom.addEventListener("mouseout", (event) => {
   event.preventDefault();
   const kmNumber = document.getElementById("km_number");
   kmNumber.innerText = "";
@@ -10,9 +10,29 @@ newRideForm.addEventListener("click", (event) => {
   fetch("https://cors-anywhere.herokuapp.com/" + `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${originsValue}&destinations=${destinationsValue}&language=fr-FR&key=AIzaSyDrZS9hR90JvI1Rj_0xQmvtQxu0FIaBvKQ`)
   .then(response => response.json())
     .then((data) => {
-      const distance_between = data.rows[0].elements[0].distance.value;
+      const distance_between = (data.rows[0].elements[0].distance.value)/1000;
       console.log(distance_between);
-      const newRideForm = document.querySelector("#new_ride");
-      newRideForm.insertAdjacentHTML("beforeend", `<p id="km_number"> ${distance_between} </p>`);
+      const buttonSave = document.querySelector(".btn-enregistrer");
+      buttonSave.insertAdjacentHTML("beforebegin", `<p id="km_number"> ${distance_between} km </p>`);
+    });
+});
+
+// Not dry - But did not manage to dry it for the moment
+const addressTo = document.querySelector("#ride_end_place");
+addressTo.addEventListener("mouseout", (event) => {
+  event.preventDefault();
+  const kmNumber = document.getElementById("km_number");
+  kmNumber.innerText = "";
+  const origins = document.getElementById("ride_start_place");
+  const originsValue = origins.value;
+  const destinations = document.getElementById("ride_end_place");
+  const destinationsValue = destinations.value;
+  fetch("https://cors-anywhere.herokuapp.com/" + `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${originsValue}&destinations=${destinationsValue}&language=fr-FR&key=AIzaSyDrZS9hR90JvI1Rj_0xQmvtQxu0FIaBvKQ`)
+  .then(response => response.json())
+    .then((data) => {
+      const distance_between = (data.rows[0].elements[0].distance.value)/1000;
+      console.log(distance_between);
+      const buttonSave = document.querySelector(".btn-enregistrer");
+      buttonSave.insertAdjacentHTML("beforebegin", `<p id="km_number"> ${distance_between} km </p>`);
     });
 });
